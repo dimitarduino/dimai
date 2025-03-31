55b64feea08e7269a0d3f05a98fdaccca2d8a906
Dimitar <dimitar@Dimitars-Laptop.local>
2025-03-31 22:16:20 +0200

    deploy plus

--- app/api/export-video/route.jsx
+++ app/api/export-video/route.jsx
@@ -1,31 +1,31 @@
-import {getServices, renderMediaOnCloudrun} from '@remotion/cloudrun/client';
+import { getServices, renderMediaOnCloudrun } from '@remotion/cloudrun/client';
 import { NextResponse } from 'next/server';
 
-
-const services = await getServices({
-  region: 'us-east1',
-  compatibleOnly: true,
-});
- 
-const serviceName = services[0].serviceName;
-
 export async function POST(req) {
-    const {inputProps} = await req.json();
-    console.log(inputProps);
-    const result = await renderMediaOnCloudrun({
-        serviceName,
-        region: 'us-east1',
-        serveUrl: process.env.GCP_SERVER_URL,
-        composition: 'shortVideo',
-        inputProps: {},
-        codec: 'h264',
-      });
-       
-      if (result.type === 'success') {
-        console.log(result.bucketName);
-        console.log(result.renderId);
-      }
+  // Fetch services inside the POST function
+  const services = await getServices({
+    region: 'us-east1',
+    compatibleOnly: true,
+  });
 
-      return NextResponse.json({"result": result?.publicUrl})
-    //   return NextResponse.json({result: downloadUrl});
+  const serviceName = services[0]?.serviceName;
+
+  const { inputProps } = await req.json();
+  console.log(inputProps);
+
+  const result = await renderMediaOnCloudrun({
+    serviceName,
+    region: 'us-east1',
+    serveUrl: process.env.GCP_SERVER_URL,
+    composition: 'shortVideo',
+    inputProps: {},
+    codec: 'h264',
+  });
+
+  if (result.type === 'success') {
+    console.log(result.bucketName);
+    console.log(result.renderId);
+  }
+
+  return NextResponse.json({ result: result?.publicUrl });
 }
