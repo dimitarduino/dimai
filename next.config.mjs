1aa99b5c0c9175329718ad749b017e4b41c8aef7
Dimitar <dimitar@Dimitars-Laptop.local>
2025-04-01 22:13:43 +0200

    Fix env loading

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
