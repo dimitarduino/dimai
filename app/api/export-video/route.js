d8fd57f1797bde9ed622c78ec1a975aa3ef26a81
Dimitar <dimitar@Dimitars-Laptop.local>
2025-06-15 15:43:29 +0200

    Updated landing page

--- app/api/export-video/route.js
+++ app/api/export-video/route.js
@@ -1,43 +1,54 @@
-import { getServices, renderMediaOnCloudrun } from '@remotion/cloudrun/client';
+import { getFunctions, renderMediaOnLambda, getRenderProgress } from '@remotion/lambda/client';
 import { NextResponse } from 'next/server';
 
 export async function POST(req) {
-  // Fetch services inside the POST function
-  const services = await getServices({
-    region: 'europe-west1',
+  const functions = await getFunctions({
+    region: 'us-east-1',
     compatibleOnly: true,
   });
 
-  const serviceName = services[0]?.serviceName;
+  const functionName = functions[0].functionName;
 
   const { inputProps } = await req.json();
-  
+
   // Calculate duration based on the provided videoData
   const captionsMs = inputProps.captions?.at(-1)?.end || 0;
   const bufferFrames = 10;
   const durationInFrames = Math.round((captionsMs / 1000) * 30) + bufferFrames;
-  console.log(inputProps.captions?.at(-1).end || 0);
-  console.log(durationInFrames);
 
-  const result = await renderMediaOnCloudrun({
-    serviceName,
-    region: 'europe-west1',
-    serveUrl: process.env.GCP_SERVER_URL,
+  const { renderId, bucketName } = await renderMediaOnLambda({
+    region: 'us-east-1',
+    functionName,
+    serveUrl: process.env.AWS_SERVE_URL,
     composition: 'shortVideo',
     inputProps: {
       videoData: inputProps,
       durationInFrames: durationInFrames
     },
     codec: 'h264',
-    durationInFrames: durationInFrames,
-    maxRetries: 3,
-    concurrency: 1,
-    framesPerLambda: 1,
+    imageFormat: 'jpeg',
+    maxRetries: 1,
+    framesPerLambda: 30,
+    privacy: 'public',
   });
 
-  if (result.type === 'success') {
-    return NextResponse.json({ result: result?.publicUrl });
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const progress = await getRenderProgress({
+      renderId,
+      bucketName,
+      functionName,
+      region: 'us-east-1',
+    });
+    if (progress.done) {
+      return NextResponse.json({ result: progress.outputFile });
+      console.log('Render finished!', progress.outputFile);
+      process.exit(0);
+    }
+    if (progress.fatalErrorEncountered) {
+      return NextResponse.json({error: "Error enountered", errors: progress.errors}, {status: 500});
+      console.log('error enountered', progress.errors);
+      process.exit(1);
+    }
   }
-
-  return NextResponse.json({ error: 'Failed to render video' }, { status: 500 });
 }
