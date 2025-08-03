39bc94abc8bd809c1ccaadc0469c61180c032383
Dimitar <dimitar@Dimitars-Laptop.local>
2025-08-03 15:02:01 +0200

    sorting, bugfix open

--- app/app/_components/VideosDashboard.jsx
+++ app/app/_components/VideosDashboard.jsx
@@ -13,35 +13,43 @@ function VideosDashboard({ videoList }) {
     const [durationFrame, setDurationFrame] = useState(0);
 
     const setOpenVideo = (id) => {
-        const videoOpened = videoList.find((video) => video.id === id); 
+        console.log('kje otvorim video', id)
+        const videoOpened = videoList.find((video) => video.id === id);
+        console.log('videoOpened', videoOpened) 
         setOpenedVideo(videoOpened);
         setOpenDialog(Date.now());
         setVideoId(id);
+        console.log('openDialog', openDialog)
+        console.log('videoId', id)
     }
 
     return (
         <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl-grid-cols-6 gap-6">
-            {videoList.map((video, index) => (
-                <div onClick={() => setOpenVideo(video.id)} key={index} className='hover:scale-110 transition-all cursor-pointer'>
-                    <Thumbnail
-                        className="rounded-xl w-full"
-                        component={RemotionVideo}
-                        compositionWidth={200}
-                        compositionHeight={320}
-                        fps={30}
-                        frameToDisplay={30}
-                        durationInFrames={120}
-                        inputProps={{
-                            videoData: {
-                                ...video
-                            },
-                            setDurationInFrame: (v) => setDurationFrame(v)
-                        }}
-                    />
-                </div>
-            ))}
+            {videoList
+                .sort((a, b) => b.id - a.id)
+                .map((video, index) => (
+                    <div onClick={() => setOpenVideo(video.id)}  className='overflow-hidden rounded-xl' key={index}>
+                        <div className='hover:scale-110 overflow-hidden transition-all cursor-pointer'>
+                            <Thumbnail
+                                className="rounded-xl w-full"
+                                component={RemotionVideo}
+                                compositionWidth={200}
+                                compositionHeight={320}
+                                fps={30}
+                                frameToDisplay={30}
+                                durationInFrames={120}
+                                inputProps={{
+                                    videoData: {
+                                        ...video
+                                    },
+                                    setDurationInFrame: (v) => setDurationFrame(v)
+                                }}
+                            />
+                        </div>
+                    </div>
+                ))}
 
-            <PlayerDialog playVideo={!!openDialog} videoId={videoId} downloadUrlProp={openedVideo ? openedVideo.downloadUrl : false} />
+            <PlayerDialog setOpenDialogPlayer={setOpenDialog} playVideo={!!openDialog} videoId={videoId} downloadUrlProp={openedVideo ? openedVideo.downloadUrl : false} />
         </div>
     )
 }
