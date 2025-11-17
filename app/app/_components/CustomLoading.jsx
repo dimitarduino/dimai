a6f421e8dc6dcbf11306d4d040cff21d373c3b4c
Dimitar <dimitar@Dimitars-Laptop.local>
2025-11-17 07:15:51 +0100

    responsive loading

--- app/app/_components/CustomLoading.jsx
+++ app/app/_components/CustomLoading.jsx
@@ -19,10 +19,10 @@ function CustomLoading({ loading, title='Generating your video' }) {
     return (
         <AlertDialog open={loading}>
             <AlertDialogTitle></AlertDialogTitle>
-            <AlertDialogContent className={`border-4 border-primary py-14 z-150`}>
+            <AlertDialogContent className={`border-4 max-w-sm border-primary py-14 z-150`}>
                 <div className=" flex flex-col items-center justify-center">
                     <Image alt="Loading" src={'/loading1.gif?50'} width={70} height={70} />
-                    <span className="text-primary text-3xl font-bold">{title}...</span>
+                    <span className="text-primary md:text-3xl font-bold text-center text-lg">{title}...</span>
                 </div>
             </AlertDialogContent>
         </AlertDialog>
