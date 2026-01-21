d03ca1d5214d644251ce2927049f952bd24314a9
Dimitar <dimitar@Dimitars-Laptop.local>
2026-01-21 11:50:54 +0100

    updated robots sitemap

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_SITE_URL || "http://dimnai.com";
 
 /**
  * Next.js App Router robots route
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://dimnai.com";
 
 /**
  * Next.js App Router sitemap route
