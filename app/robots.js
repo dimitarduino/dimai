e2674fa9de48ecdb4b3030d0b8d030a54af5a890
Dimitar <dimitar@Dimitars-Laptop.local>
2026-01-21 11:27:10 +0100

    Sitemap+Robots

---
+++ app/robots.js
@@ -0,0 +1,26 @@
+const baseUrl =
+  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
+/**
+ * Next.js App Router robots route
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata#robots
+ */
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        // Block internal / non-SEO routes
+        disallow: [
+          "/api/",
+          "/app/",
+          "/sign-in",
+          "/sign-up",
+        ],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
+
